381f789f1387f13b58b9df2818a3b2edbc40a0ab
Kawoogie <85772341+Kawoogie@users.noreply.github.com>
2024-06-11 15:04:56 -0400

    Update DebounceIntrptLib.h
    Added momentary switch capabilities.

--- DebounceIntrptLib.h
+++ DebounceIntrptLib.h
@@ -1,27 +1,28 @@
-#ifndef DEBOUNCEINTRPTLIB_H
-#define DEBOUNCEINTRPTLIB_H
+#ifndef DEBOUNCE_INTRPT_LIB_H
+#define DEBOUNCE_INTRPT_LIB_H
 
 #include "mbed.h"
 
 /**
- * @class DebounceLib
+ * @class DebounceIntrptLib
  * @brief A library for debouncing switches.
  * 
  * This library debounces a switch connected to a specified pin and triggers a callback
  * function when the switch state changes and remains stable for a specified debounce time.
  */
-class DebounceLib {
+class DebounceIntrptLib {
 public:
     /**
-     * @brief Constructor for DebounceLib.
+     * @brief Constructor for DebounceIntrptLib.
      * 
      * @param pin The pin to which the switch is connected.
      * @param debounce_time_ms The debounce time in milliseconds.
      * @param cb The callback function to be called when the switch state is stable.
+     * @param momentary Set to true if the switch is momentary, false if bi-stable.
      * 
      * @note The accepted values for debounce_time_ms are in the range of 1 to 1000 milliseconds.
      */
-    DebounceLib(PinName pin, uint32_t debounce_time_ms, Callback<void()> cb);
+    DebounceIntrptLib(PinName pin, uint32_t debounce_time_ms, Callback<void()> cb, bool momentary = false);
 
     /**
      * @brief Reads the last stable state of the switch.
@@ -37,6 +38,7 @@ private:
     volatile int _last_read;       /**< Last read state of the switch (0 or 1). */
     volatile int _last_stable_state; /**< Last stable state of the switch (0 or 1). */
     Callback<void()> _callback;    /**< Callback function to be called when the switch state is stable. */
+    bool _momentary;               /**< True if the switch is momentary, false if bi-stable. */
 
     /**
      * @brief Interrupt handler for rising edge.
@@ -54,4 +56,4 @@ private:
     void check_state();
 };
 
-#endif // DEBOUNCEINTRPTLIB_H
+#endif // DEBOUNCE_INTRPT_LIB_H
